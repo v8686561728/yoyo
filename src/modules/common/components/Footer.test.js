f62b207742b29f1d09cfdd664c871ee40fcc8669
vinod V <vinodgopal996@gmail.com>
2020-02-14 15:05:15 +0530

    footer test done

---
+++ src/modules/common/components/Footer.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow, configure } from "../../../enzyme";
+import Footer from "./Footer";
+describe("<Footer/>", () => {
+  const props = {
+    classes: "root"
+  };
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Footer />);
+  });
+  it("should render", () => {
+    expect(wrapper).toBeTruthy();
+  });
+  it("renders p tag with content", () => {
+    const wrapper = shallow(<Footer {...props} />).dive();
+    const test = wrapper.find("p").text();
+    expect(test).toEqual("©2019 YoYo, Inc. All Rights Reserved.");
+  });
+});
